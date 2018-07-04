f013545a083dda43a57304e68e5c761e8bbfaabf
Aleks <aleksanderne1@gmail.com>
2018-07-04 20:15:31 +0200

    Added the creatCalander() to draw the calendar, havent got actual drawing code yet but added the condition on how many days there are in each month

--- main.js
+++ main.js
@@ -7,45 +7,15 @@ function setMonth() {
 	let choice = select.value;
 
 	month.textContent = choice;
-/*
-	switch(choice){
-		case "January":
-			month.textContent = "January";
-			break;
-		case "February":
-			month.textContent = "February";
-			break;
-		case "March":
-			month.textContent = "March";
-			break;
-		case "April":
-			month.textContent = "April";
-			break;
-		case "May":
-			month.textContent = "May";
-			break;
-		case "June":
-			month.textContent = "June";
-			break;
-		case "July":
-			month.textContent = "July";
-			break;
-		case "August":
-			month.textContent = "August";
-			break;
-		case "September":
-			month.textContent = "September";
-			break;
-		case "October":
-			month.textContent = "October";
-			break;
-		case "November":
-			month.textContent = "November";
-			break;
-		case "December":
-			month.textContent = "December";
-			break;
-		default:
-			month.textContent = "";
-	}*/
+}
+
+function createCAlendar(){
+	let days = 31;
+	if (choice == "February")
+		days = 28;
+	if (choice == "April" || choice == "June" || choice == "September" || choice == "November")
+		days = 30;
+	for (let drawDays = 0; drawDays != days; drawDays++) {
+		
+	}
 }
