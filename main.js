c3794ea2d4adc3a981c0e50ad57c7f7465e8d971
Aleks <aleksanderne1@gmail.com>
2018-07-04 19:59:53 +0200

    Went from if else to switch statements for changing months, the problem regarding select pointing to null was because it was initiated before select was declared in index.html, moving this outside the <body> fixed the issue

--- main.js
+++ main.js
@@ -6,9 +6,44 @@ select.addEventListener('change', setMonth);
 function setMonth() {
 	let choice = select.value;
 
-	if(choice === "January"){
-		month.textContent = "January";
-	} else {
-		month.textContent = "";
+	switch(choice){
+		case "January":
+			month.textContent = "January";
+			break;
+		case "February":
+			month.textContent = "February";
+			break;
+		case "March":
+			month.textContent = "March";
+			break;
+		case "April":
+			month.textContent = "April";
+			break;
+		case "May":
+			month.textContent = "May";
+			break;
+		case "June":
+			month.textContent = "June";
+			break;
+		case "July":
+			month.textContent = "July";
+			break;
+		case "August":
+			month.textContent = "August";
+			break;
+		case "September":
+			month.textContent = "September";
+			break;
+		case "October":
+			month.textContent = "October";
+			break;
+		case "November":
+			month.textContent = "November";
+			break;
+		case "December":
+			month.textContent = "December";
+			break;
+		default:
+			month.textContent = "";
 	}
 }
