ed98ab5675bfd46642d14f0f283abfc3eb46864a
Aleks <aleksanderne1@gmail.com>
2018-07-05 22:21:40 +0200

    Fixed the bug with days not displaying, used onchange rather then onChange.. Switched it out with addEventListener instead. Added color for the 7th days to indicate sundays. TODO: add weekdays correctly regarding to real time

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 let select = document.querySelector('select');
 let h1 = document.querySelector('h1');
 let list = document.querySelector('ul');
+let date = new Date();
 
-select.onChange = function() {
+var weekday = new Array(7);
+weekday[0] = "Monday";
+weekday[1] = "Tuesday";
+weekday[2] = "wednesday";
+weekday[3] = "Thursday";
+weekday[4] = "Friday";
+weekday[5] = "Saturday";
+weekday[6] = "Sunday";
+
+
+select.addEventListener('change', function(){
 	let choice = select.value;
 	let days = 31;
-	console.log(choice);
 
 	if (choice === "February")
 		days = 28;
@@ -14,17 +24,33 @@ select.onChange = function() {
 
 
 	createCalendar(days, choice);
-}
+})
+
 
 
 function createCalendar(days, choice){
 	h1.textContent = choice;
+	let today = date.getDate();
 
 	for (let i = 1; i <= days; i++) {
-		let listItem = document.createElement('li');
-		listItem.textContent = i;
-		list.appendChild(listItem);
+		/*if(i == today) {
+			i.setAttribute("id", "today");
+		}*/
+
+
+		if(i % 7 == 0) {
+			let listItem = document.createElement('li');
+			listItem.className = "sunday";
+			listItem.textContent = i;
+			list.appendChild(listItem);
+		} else {
+			let listItem = document.createElement('li');
+			listItem.textContent = i;
+			list.appendChild(listItem);
+		}
 	}
+
 }
+
 
 createCalendar(31, "January");
