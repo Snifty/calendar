0f0431b8efd9b800465cb6a20e6262d69a8cf8c9
Aleks <aleksanderne1@gmail.com>
2018-07-06 18:12:06 +0200

    some cleanup, still having the issue with calendar duplicating

--- main.js
+++ main.js
@@ -3,25 +3,14 @@ let h1 = document.querySelector('h1');
 let list = document.querySelector('ul');
 let date = new Date();
 
-var weekday = new Array(7);
-weekday[0] = "Monday";
-weekday[1] = "Tuesday";
-weekday[2] = "wednesday";
-weekday[3] = "Thursday";
-weekday[4] = "Friday";
-weekday[5] = "Saturday";
-weekday[6] = "Sunday";
-
+let weekday = ['Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 select.addEventListener('change', function(){
 	let choice = select.value;
 	let days = 31;
 
-	if (choice === "February")
-		days = 28;
-	if (choice === "April" || choice === "June" || choice === "September" || choice === "November") 
-		days = 30;
-
+	if (choice === 'February') days = 28;
+	if (choice === 'April' || choice === 'June' || choice === 'September' || choice === 'November') days = 30;
 
 	createCalendar(days, choice);
 })
@@ -29,17 +18,18 @@ select.addEventListener('change', function(){
 
 
 function createCalendar(days, choice){
+
 	h1.textContent = choice;
 	let today = date.getDate();
 
 	for (let i = 1; i <= days; i++) {
 		/*if(i == today) {
-			i.setAttribute("id", "today");
+			document.setAttribute('id', 'today');
 		}*/
 
 		if(i % 7 == 0) {
 			let listItem = document.createElement('li');
-			listItem.className = "sunday";
+			listItem.className = 'sunday';
 			listItem.textContent = i;
 			list.appendChild(listItem);
 		} else {
@@ -52,4 +42,4 @@ function createCalendar(days, choice){
 }
 
 
-createCalendar(31, "January");
+createCalendar(31, 'January');
